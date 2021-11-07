2b61706411a3b08339af3371383e440eb3b6bcf2
willwilson <qtvhao@gmail.com>
2021-11-07 22:29:27 +0700

    use proxyTable

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const customRouter = function (req) {
-    return 'http://www.example.org'; // protocol + host
-};
-
-const options = {
+const proxyTable = {
+    'http://localhost:3000/': 'https://www.example.org/',
+}
+const myProxy = createProxyMiddleware({
     target: 'http://localhost:8000',
-    router: customRouter,
-};
-
-const myProxy = createProxyMiddleware(options);
+    router: function (req) {
+        const requestFullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        let origin = proxyTable[requestFullUrl];
+        console.log(origin)
+        return origin; // protocol + host
+    },
+    changeOrigin: true
+});
 
 const app = express();
 app.use(myProxy); // add the proxy to express
