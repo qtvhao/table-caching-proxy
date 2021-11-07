cbb97f3d900cb58d89fc481c20afc5e20c61fa4b
willwilson <qtvhao@gmail.com>
2021-11-08 00:43:22 +0700

    refresh PROXY_TABLE_REGISTRY after each 30 mins

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxyTable = {
-    'http://localhost:3000/': 'https://www.example.org/',
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const axios = require('axios')
+
+global.proxyTable = {
+    'http://localhost/': 'https://www.example.org/',
+    'http://localhost': 'https://www.example.org/',
 }
+
+function refreshProxyTable() {
+    axios(process.env.PROXY_TABLE_REGISTRY)
+        .then(function (resp) {
+            if (typeof resp.data === "string") {
+                global.proxyTable = JSON.parse(resp.data)
+            }
+            if (typeof resp.data === "object") {
+                global.proxyTable = resp.data
+            }
+            console.log(global.proxyTable)
+        })
+}
+
+refreshProxyTable()
+setInterval(refreshProxyTable, 60e3 * 30)
 const myProxy = createProxyMiddleware({
     target: 'http://localhost:8000',
     router: function (req) {
         const requestFullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-        let origin = proxyTable[requestFullUrl];
+        let origin = global.proxyTable[requestFullUrl];
         console.log(origin)
         return origin; // protocol + host
     },
@@ -17,4 +36,4 @@ const myProxy = createProxyMiddleware({
 const app = express();
 app.use(myProxy); // add the proxy to express
 
-app.listen(3000);
+app.listen(80);
